8bfc95d2a410bb7301c9e50360aea1b3acf6c23d
xhalllly <starstay@163.com>
2024-11-30 23:11:22 +0800

    archive

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+// import Hero from '@/components/Hero.vue';
+// import HomeCards from '@/components/HomeCards.vue';
+// import JobListings from '@/components/JobListings.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+
+<template>
+  <Navbar />
+  <!-- <Hero title="Test Title" subtitle="Test Subtitle" />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true" /> -->
+  <RouterView />
+</template>
